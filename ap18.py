0495c714f6b5c1120740d3c5414320407a640374
ishikasavant7-prog <ishikasavant7@gmail.com>
2025-10-29 13:34:21 +0530

    first commit

---
+++ ap18.py
@@ -0,0 +1,11 @@
+# print() function in python
+rad = 13
+areac=3.14+(rad*rad)
+# concatanation method with + symbol
+print('radius='+ str(rad)+'area of circle ='+ str(areac))
+# comma as data separator method 
+print('radius=',rad,'area of circle=',areac)
+# print () with formatted () function
+print('radius ={} area of circle ={}'.format(rad,areac))
+# print formatted string method
+print('radius={rad} area of circle={areac}')
